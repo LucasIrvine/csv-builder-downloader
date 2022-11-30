be39ef3ba0ecac53fbf8ef2738d4752e49ea8413
Lucas Irvine <lucasirvine@gmail.com>
2022-11-30 13:12:24 -0600

    Adding a basic README and the start of the CsvDownloader.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-#csv-builder-downloader
+# csv-builder-downloader
+
+## A dependancy free JavaScript tool for composing and downloading csv files. Written in TypeScript.
+
+<br />
+
+### Compose a csv file with easy to understand chainable syntax that matches the structure of your data model:
+
+```
+const {
+    defaultFileHeading = '',
+    yesterdayFileHeaders = [],
+    yesterdayData = [],
+    todayFileHeaders = [],
+    todayData = [],
+} from yourDataModelOrReducer;
+
+return new CsvBuilder() // Pass your config or not for defaults
+    .addRow(`${defaultFileHeading} Yesterday:`)
+    .addRow(yesterdayFileHeaders)
+    .addRows(yesterdayData)
+    .addNewLine(2)
+    .addSection({
+        title: `${defaultFileHeading} Today:`,
+        headers: todayFileHeaders,
+        rows: todayData,
+        newLines: 2
+    }); // or organize the same by section
+```
--- src/CsvDownloader/CsvDownloader.ts
+++ src/CsvDownloader/CsvDownloader.ts
@@ -1,3 +1,60 @@
 export default class CsvDownloader {
-  constructor() {}
+  encodedFile: string;
+  filename: string;
+
+  constructor(encodedFile: string, filename: string) {
+    this.encodedFile = encodedFile;
+    this.filename = filename;
+  }
+
+  getBlob() {
+    return new Blob([this.encodedFile]);
+  }
+
+  getBlobUrl(csvData: Blob) {
+    if (window.URL?.createObjectURL) {
+      return window.URL.createObjectURL(csvData);
+    }
+
+    return window.webkitURL.createObjectURL(csvData);
+  }
+
+  download() {
+    const downloadLink = document.createElement('a');
+
+    downloadLink.href = this.encodedFile;
+    downloadLink.target = '_blank';
+    downloadLink.download = this.filename;
+    downloadLink.click();
+  }
+
+  downloadBlob() {
+    const filename = this.filename;
+    const csvBlob = this.getBlob();
+
+    // Proprietary to IE window.navigator
+    const nav = window.navigator as any;
+
+    if (nav.msSaveBlob) {
+      nav.msSaveBlob(csvBlob, filename);
+
+      return;
+    }
+
+    // Non-IE
+    const downloadUrl = this.getBlobUrl(csvBlob);
+    const tempLink = document.createElement('a');
+
+    tempLink.style.display = 'none';
+    tempLink.href = downloadUrl;
+    tempLink.setAttribute('download', filename);
+
+    if (typeof tempLink.download === 'undefined') {
+      tempLink.setAttribute('target', '_blank');
+    }
+
+    document.body.appendChild(tempLink);
+    tempLink.click();
+    document.body.removeChild(tempLink);
+  }
 }
